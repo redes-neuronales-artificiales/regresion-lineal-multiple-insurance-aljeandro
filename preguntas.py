9da267777d79e5fecdb6eda69acf6acc0a0db93a
ALEJO\aleor <alejitoom5@gmail.com>
2022-06-26 22:19:04 -0500

    Entrega 4

--- preguntas.py
+++ preguntas.py
@@ -100,7 +100,7 @@ def pregunta_03():
             # características más importantes. Utilice la función f_regression.
             (
                 "selectKBest",
-                SelectKBest(score_func=f_regression()),
+                SelectKBest(score_func=f_regression),
             ),
             # Paso 3: Construya un modelo de regresión lineal.
             (
